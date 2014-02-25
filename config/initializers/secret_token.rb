6c6218ca5bce2865a6312a280c058d28ab99d036
John Doe <Neon_dvs@mail.ru>
2014-02-25 14:26:52 +0200

    firs commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Papa::Application.config.secret_token = '8138f60dfb34da0392042f6a8f327d0aafb602d4d47096a35e20f8d3ed614afbd130fb9118b21aa9f566b21b9e891f1fed89924447f5ebfad5464972cb3a4410'
