cf71ab6aa6314c3dbc9ba6425646eac276d02c13
John Doe <Neon_dvs@mail.ru>
2014-03-06 13:16:53 +0200

    Подключён бустрап, вроде работает

--- app/models/father.rb
+++ app/models/father.rb
@@ -1,10 +1,9 @@
 class Father < ActiveRecord::Base
  
- validates :name, :age, :surname, :job, presence: true
+ validates :name,  :surname, :job, presence: true
 
- validates :age, numericality: { less_than: 150 }
 
-attr_accessible :age, :name, :surname, :job
+attr_accessible  :name, :surname, :job
 has_many :babies
 
 
